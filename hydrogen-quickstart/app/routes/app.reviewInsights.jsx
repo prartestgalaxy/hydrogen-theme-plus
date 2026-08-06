5e0696818ae1335c99232c6cd4210b8e9cd289af
Utkarsh Koge <utkarsh.koge@galaxyweblinks.com>
2026-08-06 16:57:08 +0530

    fixes

--- hydrogen-quickstart/app/routes/app.reviewInsights.jsx
+++ hydrogen-quickstart/app/routes/app.reviewInsights.jsx
@@ -421,6 +421,9 @@ export default function ReviewInsights() {
           }}
         >
           See Demo
+          <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+          </svg>
         </a>
       </section>
 
